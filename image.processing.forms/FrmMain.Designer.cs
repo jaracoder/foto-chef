82ad6c9fd95f04dfb32b6796656e4f56eb30a1dd
Juan Antonio Ripoll <juan.ripoll.93@gmail.com>
2020-05-14 19:26:36 +0200

    Primer commite del proyecto. Esquelo básico.

---
+++ image.processing.forms/FrmMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace image.processing.forms
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveImageDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.groupRedimension = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.groupRedimension.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openImageDialog";
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(23, 19);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(170, 167);
+            this.imageBox.TabIndex = 0;
+            this.imageBox.TabStop = false;
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(23, 192);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(170, 23);
+            this.btnLoadImage.TabIndex = 1;
+            this.btnLoadImage.Text = "Subir imagen";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(407, 192);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(20, 45);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(54, 20);
+            this.txtWidth.TabIndex = 3;
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(20, 28);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(38, 13);
+            this.lblWidth.TabIndex = 4;
+            this.lblWidth.Text = "Ancho";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(100, 28);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(25, 13);
+            this.lblHeight.TabIndex = 6;
+            this.lblHeight.Text = "Alto";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(100, 45);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(54, 20);
+            this.txtHeight.TabIndex = 5;
+            // 
+            // groupRedimension
+            // 
+            this.groupRedimension.Controls.Add(this.txtHeight);
+            this.groupRedimension.Controls.Add(this.txtWidth);
+            this.groupRedimension.Controls.Add(this.lblHeight);
+            this.groupRedimension.Controls.Add(this.lblWidth);
+            this.groupRedimension.Location = new System.Drawing.Point(214, 19);
+            this.groupRedimension.Name = "groupRedimension";
+            this.groupRedimension.Size = new System.Drawing.Size(268, 87);
+            this.groupRedimension.TabIndex = 8;
+            this.groupRedimension.TabStop = false;
+            this.groupRedimension.Text = "Redimensionar imagen";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 231);
+            this.Controls.Add(this.groupRedimension);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.imageBox);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Processing";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.groupRedimension.ResumeLayout(false);
+            this.groupRedimension.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveImageDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.GroupBox groupRedimension;
+    }
+}
+
